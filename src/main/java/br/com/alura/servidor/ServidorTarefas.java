9138d97fb6dc02fcafc197b48e21023726e2fe76
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-15 08:23:41 -0300

    thread - primeira tarefa

--- src/main/java/br/com/alura/servidor/ServidorTarefas.java
+++ src/main/java/br/com/alura/servidor/ServidorTarefas.java
@@ -1,12 +1,12 @@
 package br.com.alura.servidor;
 
-import java.io.IOException;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorTarefas {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         System.out.println("---- Iniciando Servidor ----");
         ServerSocket servidor = new ServerSocket(12345);
@@ -14,6 +14,10 @@ public class ServidorTarefas {
         while (true) {
             Socket socket = servidor.accept();
             System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+
+            DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
+            Thread threadCliente = new Thread(distribuirTarefas);
+            threadCliente.start();
         }
     }
 }
